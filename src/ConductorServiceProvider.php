85021713fee81ae6e761407f07c796fe4a1b7db9
Rapolas Gruzdys <polasgruzdys@gmail.com>
2024-03-24 20:23:25 +0200

    Cleanup - Remove the placeholder ModelFactory.php - Remove views configuration in ConductorServiceProvider.php

--- src/ConductorServiceProvider.php
+++ src/ConductorServiceProvider.php
@@ -9,14 +9,8 @@ class ConductorServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-conductor')
-            ->hasConfigFile()
-            ->hasViews();
+            ->hasConfigFile();
     }
 }
