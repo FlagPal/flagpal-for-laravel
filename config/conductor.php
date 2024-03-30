d1e034e33420bb41ade8ee3d97212aabc801747e
Rapolas Gruzdys <polasgruzdys@gmail.com>
2024-03-30 20:40:32 +0200

    Remove comment in config

--- config/conductor.php
+++ config/conductor.php
@@ -1,14 +1,6 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Base URI
-    |--------------------------------------------------------------------------
-    |
-    | Specify a base uri which will be prepended to every URI.
-    |
-    */
     'base_uri' => env('CONDUCTOR_URL'),
     'bearer_token' => env('CONDUCTOR_TOKEN'),
 ];
