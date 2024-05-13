ce1c84d5fb8b2939a71a74d324c93b1b025ce590
rapkis <rapkis@users.noreply.github.com>
2024-05-13 18:39:26 +0000

    Fix styling

--- src/Validation/RuleFactory.php
+++ src/Validation/RuleFactory.php
@@ -9,7 +9,7 @@ class RuleFactory
 {
     public function make(string $name): Rule
     {
-        $className = __NAMESPACE__."\\Rules\\" . Str::studly($name) . 'Rule';
+        $className = __NAMESPACE__.'\\Rules\\'.Str::studly($name).'Rule';
         if (! class_exists($className)) {
             throw new \InvalidArgumentException("Rule \"{$name}\" does not exist");
         }
