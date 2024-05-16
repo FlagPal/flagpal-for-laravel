ece149acf8993d2a1264463ca0df644ea486297f
Rapolas Gruzdys <polasgruzdys@gmail.com>
2024-05-16 19:19:49 +0300

    Upgrade "swisnl/json-api-client" from ^2.3 (2.3.2) to ^2.4 (2.4.0) - The new version accepts header arrays in repositories - This allowed to remove the FetchMany.php re-implementation introduced in commit b61bbc1833d10019b026aa89ec0fce5584eaca50

--- src/Repositories/FunnelRepository.php
+++ src/Repositories/FunnelRepository.php
@@ -2,7 +2,7 @@
 
 namespace Rapkis\Conductor\Repositories;
 
-use Rapkis\Conductor\Client\Actions\FetchMany;
+use Swis\JsonApi\Client\Actions\FetchMany;
 use Swis\JsonApi\Client\BaseRepository;
 
 class FunnelRepository extends BaseRepository
